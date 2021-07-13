852d614b195353fe6918b9d56783395eb53e9638
Chingiz Agha <cingiz167@gmail.com>
2021-07-13 12:06:39 +0400

    Some Models added and front side improved

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,3 +8,29 @@ class Profile(db.Model):
      location = db.Column(db.String(80), nullable=False)
      desc = db.Column(db.Text, nullable=False)
      image = db.Column(db.String(20), nullable=True)
+
+class Education(db.Model):
+
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(40), nullable=False)
+     date = db.Column(db.String(30), nullable=False)
+     title = db.Column(db.String(80), nullable=False)
+     desc = db.Column(db.Text, nullable=False)
+     place = db.Column(db.String(40), nullable=False)
+
+class Career(db.Model):
+
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(40), nullable=False)
+     date = db.Column(db.String(30), nullable=False)
+     title = db.Column(db.String(80), nullable=False)
+     desc = db.Column(db.Text, nullable=False)
+     place = db.Column(db.String(40), nullable=False)
+     site = db.Column(db.String(50), nullable=True)
+
+
+
+
+
+
+
